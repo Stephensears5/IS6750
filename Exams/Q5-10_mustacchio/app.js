1267c7aecf00ad120b5d630dc719f04fe8606664
Stephensears5 <a01234875@aggies.usu.edu>
2023-03-03 17:24:18 -0700

    finished Q4-10

--- Exams/Q5-10_mustacchio/app.js
+++ Exams/Q5-10_mustacchio/app.js
@@ -5,23 +5,39 @@ const path = require('path');
 const express = require('express');
 const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
+const sequelize = require("./util/database");
+const morgan = require('morgan');
+const fs = require('fs');
+
+
+//Routes
 const homeRoutes = require('./routes/homeRoutes')
 const stylesRoutes = require('./routes/stylesRoutes')
-const sequelize = require("./util/database");
-const MustacheStyles = require("./models/mustacheStyles");
+const blogRoutes = require('./routes/blogRoutes')
+const contactRoutes = require('./routes/contactRoutes')
 // Import local modules
 
 const homeController = require('./controllers/homeController');
-const stylesController = require('./controllers/stylesController')
+const stylesController = require('./controllers/stylesController');
+const blogController = require('./controllers/blogController');
+const MustacheStyles = require("./models/mustacheStyles");
+
+//Logs
+const logRequests = require('./middleware');
+
+
 
 // Initialize our app
 const app = express();
 
 // Load middleware to point to static resources
 app.use(express.static(path.join(__dirname, 'public/')));
+var accessLogStream = fs.createWriteStream(path.join(__dirname, '/logs/logRequests.txt'), { flags: 'a' })
+ 
 
 //Load Middleware to parse request body
 app.use(express.urlencoded({extended: false}))
+app.use(morgan('dev', {stream: accessLogStream}))
 
 // Set the templating engine using app.set
 app.set('view engine', 'ejs');
@@ -33,8 +49,9 @@ app.use(expressLayouts);
 
 app.use(homeRoutes)
 app.use('/styles', stylesRoutes)
-
-
+app.use(blogRoutes)
+app.use(contactRoutes)
+app.use(logRequests);
 
 //Define Relationships between models
 
@@ -49,6 +66,9 @@ sequelize
   })
   // .then(x => {
   //   return stylesController.bulkAddStyles();
+  // })
+  // .then(x => {
+  //   return blogController.bulkAddBlogPosts();
   // })
   .then(x => {
     app.listen(3000);
